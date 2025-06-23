598533b82557594674b2acf495b9232c55ba39a0
parsivale313 <aminmohammadgheibi@gmail.com>
2025-06-23 14:09:53 -0700

    Initial commit for Laravel 11 project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'amir',
+            'email' => 'amir@gmail.com',
+            'password' => Hash::make('1234567890'),
+ 
+         ]);
+    }
+}
